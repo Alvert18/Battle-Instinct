506bf39771f7fa1cad643979b15bb4b7ac6ac5fc
Student <student@lab-240-012.cs.csubak.edu>
2018-10-02 11:39:48 -0700

    Added changes to albertoA.cpp

--- albertoA.cpp
+++ albertoA.cpp
@@ -26,4 +26,24 @@ void show_credits_Alberto (int x, int y)
 
 }
 
+void show_credits_Alberto_pic (int x, int y,float tx, float ty, GLuint texid) 
+{
+     
+     int wid =40;
+     y = y -40;
+     glPushMatrix();
+     glTranslatef(x-100,y-40,0);
+     glColor3f(1.0, 1.0, 1.0);
+     glBindTexture(GL_TEXTURE_2D, texid);
+     glBegin(GL_QUADS);
+        glTexCoord2f(tx, ty+.5); glVertex2i(-wid, -wid);
+        glTexCoord2f(tx, ty);    glVertex2i(-wid, wid);
+        glTexCoord2f(tx+.125,ty);    glVertex2i(wid, wid);
+        glTexCoord2f(tx+.125,ty+.5); glVertex2i(wid, -wid);
+     glEnd();
+     glPopMatrix();
+     glBindTexture(GL_TEXTURE_2D, 0);
+     glDisable(GL_ALPHA_TEST);
 
+
+}
