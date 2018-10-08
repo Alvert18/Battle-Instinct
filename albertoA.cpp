e69ce99196bd0ed4935cd4a13c0ef90fe7dec9fa
Alberto Acevedo <aacevedo@odin.cs.csubak.edu>
2018-10-07 21:49:38 -0700

    Changes to albertoA.cpp

--- albertoA.cpp
+++ albertoA.cpp
@@ -4,18 +4,10 @@
  * Individual file for:
  * - Showing credits
  * - Showing picture in credits
-*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
+ */
 #include <math.h>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
 #include <GL/glx.h>
 #include "fonts.h"
-#include <string>
 
 using namespace std;
 
@@ -34,25 +26,25 @@ void show_credits_Alberto (int x, int y)
 //Post picture on the credit screen
 void showAlbertoPicture (int x, int y,float tx, float ty, GLuint texid) 
 {
-     int wid = 40;
-     static float fx = 0.0f;
-     static float angle = 0.0f;
+    int wid = 40;
+    static float fx = 0.0f;
+    static float angle = 0.0f;
 
-     angle += 0.01;
-     fx = sin(angle);
-     y = y -40;
+    angle += 0.01;
+    fx = sin(angle);
+    y = y -40;
 
-     glPushMatrix();
-     glTranslatef(x + (int)(fx*30.0),y,0);
-     glColor3f(1.0, 1.0, 1.0);
-     glBindTexture(GL_TEXTURE_2D, texid);
-     glBegin(GL_QUADS);
+    glPushMatrix();
+    glTranslatef(x + (int)(fx*30.0),y,0);
+    glColor3f(1.0, 1.0, 1.0);
+    glBindTexture(GL_TEXTURE_2D, texid);
+    glBegin(GL_QUADS);
         glTexCoord2f(tx, ty+.5);     glVertex2i(-wid, -wid);
         glTexCoord2f(tx, ty);        glVertex2i(-wid, wid);
         glTexCoord2f(tx+.125,ty);    glVertex2i(wid, wid);
         glTexCoord2f(tx+.125,ty+.5); glVertex2i(wid, -wid);
-     glEnd();
-     glPopMatrix();
-     glBindTexture(GL_TEXTURE_2D, 0);
-     glDisable(GL_ALPHA_TEST);
+    glEnd();
+    glPopMatrix();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glDisable(GL_ALPHA_TEST);
 }
